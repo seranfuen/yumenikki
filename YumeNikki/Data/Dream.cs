08d468f9fc229794b507edba5caf414415ace6ef
Sergio Ángel Verbo <seranfuen@gmail.com>
2016-04-16 20:40:20 +0200

    Initial

---
+++ YumeNikki/Data/Dream.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YumeNikki.Data
+{
+    public class Dream
+    {
+        public string Contents { get; set; }
+        public int Id { get; set; }
+        public DateTime DreamDate { get; set; }
+        public DateTime DreamAddedDate { get; set; }
+        public Dictionary<DreamCategory, List<DreamTag>> DreamCategoryTags { get; }
+    }
+}
---
+++ YumeNikki/Windows/DreamLayout.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using YumeNikki.Data;
+
+namespace YumeNikki.Windows
+{
+    /// <summary>
+    /// Interaction logic for DreamLayout.xaml
+    /// </summary>
+    public partial class DreamLayout : Window
+    {
+        public DreamLayout()
+        {
+            InitializeComponent();
+            this.Test();
+        }
+
+        public void Test()
+        {
+            var dream = new Dream()
+            {
+                DreamDate = new DateTime(2015, 1, 2)
+            };
+            DreamLayoutControl.DataContext = dream;
+        }
+    }
+}
